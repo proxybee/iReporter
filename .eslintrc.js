2a8477d0aa35d38b0396659299547d68dd24935e
Elizabeth Agoyi <inspireworldsyouth@gmail.com>
2018-11-27 10:57:54 -0800

    linting library and babel style guide set-up

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = { 
+    "extends": ["airbnb-base"],
+    "env": {
+      "es6": true,
+      "node": true
+    },
+    "rules": {
+      "semi": "error",
+      "no-var": 2,
+      "prefer-arrow-callback": "error"
+    } 
+  };
