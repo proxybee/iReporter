538bd80b1ca577002eab2c584d8054df4a5615ad
proxybee <inspireworldsyouth@gmail.com>
2020-05-28 18:12:27 -0700

    Fix bugs for heroku deploy

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,6 +4,22 @@ if (localStorage.userToken) {
   window.location = `userp.html?id=${localStorage.id}` || `admin.html?id=${localStorage.id}`;
 }
 
+// // Name and Email validation Function.
+// function validation(){
+// var name = document.getElementById("name").value;
+// var email = document.getElementById("email").value;
+// var emailReg = /^([w-.]+@([w-]+.)+[w-]{2,4})?$/;
+// if( name ==='' || email ===''){
+// alert("Please fill all fields...!!!!!!");
+// return false;
+// }else if(!(email).match(emailReg)){
+// alert("Invalid Email...!!!!!!");
+// return false;
+// }else{
+// return true;
+// }
+// }
+
   function submitSignUp(e) {
   e.preventDefault()
 
@@ -14,7 +30,7 @@ if (localStorage.userToken) {
     email: document.getElementById('email').value,
     phone_number: document.getElementById('phone_number').value,
     password: document.getElementById("password").value,
-    
+
   }
     fetch('https://ireporterafrica.herokuapp.com/api/v1/users/signup', {
     method: "POST",
